9830ff5f6bbf56a0bf6aa2c04306e0a51778722b
Janani-24 <114230940+Janani-24@users.noreply.github.com>
2022-11-29 23:44:18 -0500

    Assignment 10
    Assignment 10

---
+++ Assignment 10/Task 2 weather-app/weather-app/src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "464776b842msh9fdddde0b3f84c0p1494e3jsn15b1f2b5916a",// enter your rapid api key here
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "f68e5d2a3489633969c8b6e7ea749c79"; // enter your key from openweather API
